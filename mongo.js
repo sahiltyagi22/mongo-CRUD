52ede9688d9538379d43aa8a575134603f9e8174
sahil <sahiltyagi1204@outlook.com>
2023-03-20 19:42:01 +0530

    mongo crud code

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/testDatabase')
+.then(()=>
+    console.log("connection is successul"))
+.catch(err=> console.error("could not connect to the server . err"))    
+
+const courseSchema =  new mongoose.Schema({
+        name : String,
+        courseId : Number,
+        Tutor : String,
+        PublishStatus : Boolean
+})
+
+const   Course = mongoose.model('Course' ,courseSchema)
+
+// async function createCourse (){
+//     const course = new Course({
+//         name :"course1",
+//         courseId :3,
+//         Tutor :'sahil',
+//         PublishStatus : true
+  //  })
+    
+    // const result = await course.save()
+
+// }
+
+async function getCourses (){
+    const courses = await Course.find({Tutor : 'sahil'})
+    console.log(courses);
+}
+
+
+// createCourse()
+getCourses()
+
